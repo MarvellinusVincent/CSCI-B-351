8a6c0529ed4e588fc64d000711c2be77e8a6208a
Marvell <vmarvellinus@gmail.com>
2023-02-08 17:02:02 -0500

    commit

---
+++ a1/grading_b351/HW1/Yang Wang/a1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# B351/Q351 SP 2023
+# Do not share these assignments or their solutions outside of this class.
+
+#################################
+#                               #
+# Assignment 1: Python Methods  #
+#                               #
+#################################
+
+
